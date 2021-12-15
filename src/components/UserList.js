811df9ecf4b082d56886666c10fe3414cbbaf699
Choi43 <rqdda11@gmail.com>
2021-12-15 15:52:25 +0900

    add user info Link

--- src/components/UserList.js
+++ src/components/UserList.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const userList = ({ users }) => {
   return (
@@ -10,7 +11,7 @@ const userList = ({ users }) => {
             className="card mb-2" 
             key={user.id}>
             <div className="card-body p-3">
-              {user.name}
+              <Link to={`/users/${user.id}`}>{user.name}</Link>
             </div>
           </div>
         );
